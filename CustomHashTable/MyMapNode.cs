4f7c21837d4ccc3c67af08c08a4c2aebed2f223a
Diwakar <diwakarn1599@gmail.com>
2021-07-12 14:20:23 +0530

    [Diwakar] getting frequencies for words in linked list

---
+++ CustomHashTable/MyMapNode.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomHashTable
+{
+    //structure for key value 
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+    class MyMapNode<K,V>
+    {
+        public int size;
+        LinkedList<KeyValue<K, V>>[] items;
+        
+        //constructure for initializing linked list
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        //method for getting array postion
+        public int GetArrayPosition(K Key)
+        {
+            int pos = Key.GetHashCode() % this.size;
+            return Math.Abs(pos);
+        }
+
+        //getting linked lists for particular key
+        public LinkedList<KeyValue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> list = items[position];
+            if (list == null)
+            {
+                list = new LinkedList<KeyValue<K, V>>();
+                items[position] = list;
+            }
+            return list;
+        }
+
+        //adding into the hastable
+        public void Add(K Key,V Value)
+        {
+            int pos = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(pos);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = Key, Value = Value };
+            linkedList.AddLast(item);
+        }
+
+        //checking whether the elements is already present or not
+        public int Check(K Key)
+        {
+            int pos = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(pos);
+            int count = 1;
+            bool found = false;
+            KeyValue<K, V> foundKey = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> keyVal in linkedList)
+            {
+                if (keyVal.Key.Equals(Key))
+                {
+                    count = Convert.ToInt32(keyVal.Value) + 1;
+                    found = true;
+                    foundKey = keyVal;
+                }
+            }
+
+                if (found)
+                {
+                    linkedList.Remove(foundKey);
+                    return count;
+                }
+                else
+                {
+                    return 1;
+                }
+                
+            
+        }
+
+        //get values based on key
+        public V Get(K Key)
+        {
+            int pos = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> list = GetLinkedList(pos);
+            foreach (KeyValue<K, V> keyValue in list)
+            {
+                if (keyValue.Key.Equals(Key))
+                {
+
+                    return keyValue.Value;
+                }
+            }
+            return default(V);
+        }
+    }
+}
--- CustomHashTable/Program.cs
+++ CustomHashTable/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CustomHashTable
 {
@@ -7,6 +9,35 @@ namespace CustomHashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Custom Hash Table");
+            //string to be set in hashtable
+            string str = "To be or not to be".ToLower();
+            //converting string to array
+            string[] arr = str.Split();
+
+            //set for getting unique elements in array
+            HashSet<string> set = new HashSet<string>(arr);
+            int count;
+            //object for mymap class
+            MyMapNode<string, int> map = new MyMapNode<string, int>(arr.Length);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                count = map.Check(arr[i]);
+                if (count > 1)
+                {
+                    map.Add(arr[i], count);
+                }
+                else
+                {
+
+                    map.Add(arr[i], 1);
+                }
+            }
+           
+            //printing values with frequency
+            foreach(var i in set)
+            {
+                Console.WriteLine($"{i}---> {map.Get(i)}");
+            }
         }
     }
 }
